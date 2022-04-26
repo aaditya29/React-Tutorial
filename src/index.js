6b854211aa7243647e72ecc1840d100d1e457a63
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-26 16:17:56 +0530

    First Component Added

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Creating First Component
+import React from 'react'
+
+function Greeting(){//by capitalising the first letter now react knows it is an special component.
+    return <h4>This is Aditya and i am here making my first component</h4>
+}// Add this functionality in index.html
