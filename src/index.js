abfc79159f2dddba8305dbe74ee56f2a8f5e869e
Aditya Mishra <mishraaaditya29@gmail.com>
2022-04-26 16:24:59 +0530

    First Component Made

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // Creating First Component
 import React from 'react'
+import ReactDom from 'react-dom'
 
 function Greeting(){//by capitalising the first letter now react knows it is an special component.
     return <h4>This is Aditya and i am here making my first component</h4>
 }// Add this functionality in index.html
+
+ReactDom.render(<Greeting />, document.getElementById('root'))// This function sees two things, what we are going to render and where we want it to be rendered.
