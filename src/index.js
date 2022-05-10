0a82c5884eee9c61d7a7b525a01745672bd7fc87
Aditya Mishra <mishraaaditya29@gmail.com>
2022-05-10 18:58:47 +0530

    git commit

--- src/index.js
+++ src/index.js
@@ -34,12 +34,13 @@ function BookList(){
     )
 }
 
-const Book = (props)=> {
+const Book = ({img, title, author}) => {
+    //const {img, title, author } = props;
     return (
     <article className="book">
-        <img src={props.img} alt="" />
-        <h2>{props.title}</h2>
-        <h4>{props.author}</h4>
+        <img src={img} alt="" />
+        <h2>{title}</h2>
+        <h4>{author}</h4>
     </article>
     )
 }
